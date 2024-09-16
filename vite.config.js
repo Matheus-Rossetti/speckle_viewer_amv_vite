77565cbfa4b4d4c590326b1b6e95dcf4303e591e
Matheus-Rossetti <127154497+Matheus-Rossetti@users.noreply.github.com>
2024-09-16 13:36:49 -0300

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-    base: '/speckle_viewer_amv_vite/', // Altere para o nome do seu repositório
-    // Outras configurações
-});
