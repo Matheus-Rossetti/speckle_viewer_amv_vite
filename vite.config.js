300db1a2db7420c5cc9a5585bc674511a0390fae
Matheus-Rossetti <matheuzdreher@gmail.com>
2024-09-16 12:44:02 -0300

    segundo commit

---
+++ vite.config.js
@@ -0,0 +1,6 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    base: '/speckle_viewer_amv_vite/', // Altere para o nome do seu repositório
+    // Outras configurações
+});
